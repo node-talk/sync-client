17e51c74f3d706db000fcca087f93665e0d79171
Dom <dominique.prunier@gmail.com>
2013-04-06 23:59:12 -0400

    better support for multiple registrations, replaying creates when listeners register

--- monitor.js
+++ monitor.js
@@ -5,48 +5,58 @@ var watchr = require('watchr'),
 
 /**
  * Monitors a location for file creation or deletion. The method returns an
- * event emitter which sends 2 different events:
+ * event emitter which sends 2 different events with the path and the file stat:
  *  - 'create': when a new file is created
  *  - 'delete': when a file is deleted
- * Note that upon initialization, the monitor sends a 'create' event for every
+ * Note that upon registration, the monitor sends a 'create' event for every
  * existing file.
  *
  * @param {string} the location to monitor
  * @return {EventEmitter} the monitor
  */
 module.exports.monitor = function(location) {
-    var e = new events.EventEmitter();
     location = path.normalize(location);
+    var e = new events.EventEmitter();
 
-    // some callbacks are synchronous, postpone everything to give a chance to register
-    process.nextTick(function() {
-        e.watchr = watchr.watch({
-            path: location,
-            listeners: {
-                change: function(change, path, cstat, pstat) {
-                    var stat = cstat || pstat;
-                    if ((change === 'create' || change === 'delete') && stat.isFile()) {
-                        // a file has been created or deleted !
-                        e.emit(change, path, stat);
-                    }
+    // watch location
+    e.files = {};
+    e.watchr = watchr.watch({
+        path: location,
+        listeners: {
+            change: function(change, file, cstat, pstat) {
+                file = path.relative(location, file);
+                if (change === 'create' && cstat.isFile()) {
+                    e.files[file] = cstat;
+                    e.emit(change, file, cstat);
                 }
-            },
-            next: function(err, watchers) {
-                if (err !== null || watchers === null || watchers.length === 0) {
-                    // watch didn't work...
-                    e.emit('error', new Error('monitor failed, most likely ' + location + ' doesn\'t exist !'));
-                }
-                else {
-                    // we're ready to go !
-                    e.emit('ready', location, null);
-                    // emit initial create event for existing files !
-                    walker.walk(location).on('file', function(root, stat, next) {
-                        e.emit('create', path.join(root, stat.name), stat);
-                        next();
-                    });
+                else if (change === 'delete' && pstat.isFile()) {
+                    delete e.files[file];
+                    e.emit(change, file, pstat);
                 }
             }
-        });
+        },
+        next: function(err, watchers) {
+            if (err !== null || watchers === null || watchers.length === 0) {
+                e.emit('error', new Error('monitor failed, most likely ' + location + ' doesn\'t exist !'));
+            }
+            else {
+                walker.walk(location).on('file', function(root, stat, next) {
+                    var file = path.relative(location, path.join(root, stat.name));
+                    e.files[file] = stat;
+                    e.emit('create', file, stat);
+                    next();
+                });
+            }
+        }
+    });
+
+    // when a new listener registers, replay 'create' events
+    e.on('newListener', function(event, listener) {
+        if (event === 'create') {
+            for (var file in e.files) {
+                listener(file, e.files[file]);
+            }
+        }
     });
 
     return e;
