22c24e0fa3c7cc4350526bea4b9fbcf7b5fce67b
Dom <dominique.prunier@gmail.com>
2013-04-13 00:02:18 -0400

    basic server binding socket to a monitor and serving files from this folder

---
+++ server.js
@@ -0,0 +1,58 @@
+var express = require('express'),
+    http = require('http'),
+    socketio = require('socket.io'),
+    monitor = require('./monitor'),
+    log = require('winston'),
+    path = require('path');
+
+// setup cli
+var args = require('optimist').usage('Usage: node server.js --root <root folder>').demand('root').describe('root', 'the folder to synchronize files from').argv;
+
+// create and setup app
+var app = express();
+
+app.use(express.logger());
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser());
+//app.use(express.session());
+app.use(app.router);
+app.use(express.errorHandler());
+
+// create and setup server
+var server = http.createServer(app);
+
+// create and setup socket
+var io = socketio.listen(server);
+
+io.enable('browser client minification');
+io.enable('browser client etag');
+io.enable('browser client gzip');
+io.set('log level', 1);
+
+// start server
+server.listen(process.env.PORT || 8888);
+
+// create monitor
+log.info('monitoring', args.root);
+var files = monitor.monitor(args.root);
+
+// bind socket to monitor
+io.on('connection', function(socket) {
+    ['create', 'delete', 'update'].forEach(function(event) {
+        var listener = socket.emit.bind(socket, event);
+
+        // listen for event
+        files.on(event, listener);
+
+        // remove listener on disconnect
+        socket.on('disconnect', function() {
+            files.removeListener(event, listener);
+        });
+    });
+});
+
+// serve files
+app.get('/*', function(req, res) {
+    res.sendfile(path.join(args.root, req.path));
+});
