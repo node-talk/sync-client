30ed59c0bd408113dae8efa1263e3b149bd31746
Dom <dominique.prunier@gmail.com>
2013-04-20 02:13:08 -0400

    log socket.io client errors

--- client.js
+++ client.js
@@ -1,5 +1,6 @@
 var socketio = require('socket.io-client'),
     downloader = require('./downloader'),
+    log = require('winston'),
     url = require('url');
 
 // setup cli
@@ -9,7 +10,9 @@ var args = require('optimist').usage('Usage: node server.js --server <server url
     .describe('root', 'the root folder to synchronize files to').argv;
 
 // connect to server
-var io = socketio.connect(args.server);
+var io = socketio.connect(args.server).on('error', function(err) {
+    log.warn('failed to initialize socket.io connection', err);
+});
 
 // setup file downloader
 var downloader = downloader.downloader(args.root);
