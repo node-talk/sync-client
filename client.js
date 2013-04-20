5790ece2bcc9daa24a1d24b279fe0dce6c0dafd3
Dom <dominique.prunier@gmail.com>
2013-04-19 22:39:28 -0400

    client is now only considering update events, since file is most likely incomplete on create

--- client.js
+++ client.js
@@ -15,15 +15,13 @@ var io = socketio.connect(args.server);
 var downloader = downloader.downloader(args.root);
 
 // bind change events to downloader
-['create', 'update'].forEach(function(event) {
-    io.on(event, function(file, stat) {
-        // fix times as socket.io converts them to strings
-        [ "atime", "mtime", "ctime" ].forEach(function(time) {
-            if (typeof stat[time] === 'string') {
-                stat[time] = new Date(stat[time]);
-            }
-        });
-        // download file
-        downloader.download(url.resolve(args.server, file), stat);
+io.on('update', function(file, stat) {
+    // fix times as socket.io converts them to strings
+    ["atime", "mtime", "ctime"].forEach(function(time) {
+        if (typeof stat[time] === 'string') {
+            stat[time] = new Date(stat[time]);
+        }
     });
+    // download file
+    downloader.download(url.resolve(args.server, file), stat);
 });
